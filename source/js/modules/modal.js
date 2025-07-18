fc1207b9ab32ab0ab4b45caced7dba5586715ab6
jar <dev.bazylev@gmail.com>
2025-07-18 10:59:52 +0300

    feat: add modal styles

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,17 +1,40 @@
 const initModal = () => {
   const overlay = document.querySelector('.overlay');
   const modal = document.querySelector('.modal');
+  const cross = document.querySelector('.modal__cross');
+  const cart = document.querySelector('.header__cart')
 
-  // setTimeout(() => {
-  //   console.log(document.querySelectorAll('.info__btn'));
-  // }, 2222);
+  const onMissClick = (evt) => {
+    if (!evt.target.closest('.modal')) {
+      modal.classList.remove('modal--opened');
+      overlay.classList.remove('overlay--active');
+      document.removeEventListener('click', onMissClick);
+    }
+  };
 
-  document.querySelectorAll('.info__btn').forEach(button => {
-    button.addEventListener('click', () => {
-      const card = button.closest('.info__item');
-      const id = card.getAttribute('data-id');
-    });
-  });
+  const onCart = () => {
+    modal.classList.add('modal--opened');
+    overlay.classList.add('overlay--active')
+    setTimeout(() => {
+      document.addEventListener('click', onMissClick);
+    }, 500);
+  };
+
+  const onCross = () => {
+    modal.classList.remove('modal--opened');
+    overlay.classList.remove('overlay--active')
+    document.removeEventListener('click', onMissClick);
+  };
+
+  // document.querySelectorAll('.info__btn').forEach(button => {
+  //   button.addEventListener('click', () => {
+  //     const card = button.closest('.info__item');
+  //     const id = card.getAttribute('data-id');
+  //   });
+  // });
+
+  cart.addEventListener('click', onCart);
+  cross.addEventListener('click', onCross);
 };
 
 export {initModal};
